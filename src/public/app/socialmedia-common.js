e85a662bc6032502ae6723952fb13d39918d6dc6
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-01-11 00:24:41 +0800

    added header,footer and anon user login
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- src/public/app/socialmedia-common.js
+++ src/public/app/socialmedia-common.js
@@ -1,5 +1,31 @@
 //building in components but on jquery
-$(
-    console.log('hello'),
-    $('#navbar').load('./components/navbar.html')
-)
+$(()=>{
+    
+    $('#navbar').load('./components/navbar.html',loginIfNeeded)
+    $('#footer').load('./components/footer.html')
+
+     
+})
+
+function loginIfNeeded(){
+    function updateUserNameInNav(user){
+        $('.nav-username').text(user)
+    }
+    let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null;
+
+    if(!currentUser){
+        $.post('/api/users',{},(user) =>{
+            if(user)
+            {
+                window.localStorage.user = JSON.stringify(user.username)
+                alert('registered current user as '+window.localStorage.user)
+                currentUser = user
+            }
+            
+        })
+    }
+    else{
+        alert("resuming session as "+currentUser.username)
+    }
+    updateUserNameInNav(currentUser.username)
+}
