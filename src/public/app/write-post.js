6aefcef99ddf69f0fe64d8c6ed1c14362dd6bb57
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-01-12 15:52:15 +0800

    added write post funcitionality
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

---
+++ src/public/app/write-post.js
@@ -0,0 +1,20 @@
+$(() => {
+    let postForm = document.getElementById("post-form")
+    let postTitle = postForm.elements['post-title']
+    let postBody = postForm.elements['post-body']
+
+    let btnSubmit = $('#btn-submit')
+    btnSubmit.click(() => {
+        $.post('/api/posts', {
+                id:null,
+                title: postTitle.value,
+                body: postBody.value,
+                userId:window.currentUser.id,
+
+            })
+            .done(function (data) {
+                alert("added post");
+            })
+
+    })
+})
